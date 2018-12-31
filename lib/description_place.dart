931bb4c88369929e6bdbaa41880e80cdcb1e1f8c
Carlos Perez <legomarth@hotmail.com>
2018-12-31 13:57:08 -0500

    - Se implementa la descripción del viaje - Se agrega fuente lato-regular

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -2,8 +2,51 @@ import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
 
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  //Constructor
+  DescriptionPlace(
+      this.namePlace,
+      this.stars,
+      this.descriptionPlace);
+
   @override
   Widget build(BuildContext context) {
+
+    final star_border = Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star_half = Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2C611),
+      ),
+    );
+
+    final star = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: Icon(
+        Icons.star,
+        color: Color(0xFFf2C611),
+      ),
+    );
 
     final title_stars = Row(
       children: <Widget>[
@@ -15,18 +58,48 @@ class DescriptionPlace extends StatelessWidget {
           ),
 
           child: Text(
-            "Duwili Ella",
+            namePlace,
             style: TextStyle(
               fontSize: 30.0,
               fontWeight: FontWeight.w900
             ),
             textAlign: TextAlign.left,
            ),
+        ),
+        Row(
+          children: <Widget>[
+            star,
+            star,
+            star,
+            star,
+            star_half
+          ],
         )
       ],
     );
 
-    return null;
+    final description = Container(
+      margin: new EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0
+      ),
+      child: new Text(
+        descriptionPlace,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
+        ),),
+    );
+
+
+    return Column(
+      children: <Widget>[
+        title_stars,
+        description
+      ],
+    );
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trips_app/description_place.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,16 +27,9 @@ class MyApp extends StatelessWidget {
           appBar : AppBar(
             title: Text("Hola mundo"),
             backgroundColor: Color.fromRGBO(81, 66, 154, 1),
-            leading: Icon(Icons.arrow_back),
           ),
-          body:
-            Center(
-                  child: Text(
-                    'Hola Mundo',
-                    style: TextStyle(fontWeight: FontWeight.bold)
-                ),
-              ),
-            ),
+          body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
+            )
     );
   }
 }
